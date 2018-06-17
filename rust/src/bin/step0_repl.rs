f66895808033da9894c4badd4dcd40f3c2af5bfa
Tim Morgan <tim@timmorgan.org>
2018-06-17 11:19:37 -0500

    Add readline support

--- rust/src/bin/step0_repl.rs
+++ rust/src/bin/step0_repl.rs
@@ -1,18 +1,67 @@
-use std::io::{stdin, stdout};
-use std::io::Write;
-use std::process;
+extern crate rustyline;
 
+use std::io::{stdin, stdout, Write};
+use std::env;
+use rustyline::{Editor, error::ReadlineError};
+
+const HISTORY_FILE: &str = ".mal-history";
+ 
 fn main() {
+    let mut rl = Editor::<()>::new();
+    rl.load_history(HISTORY_FILE).unwrap_or(());
+    let use_readline = use_readline();
     loop {
-        print!("user> ");
-        stdout().flush().unwrap();
-        let mut input=String::new();
-        stdin().read_line(&mut input).unwrap();
-        if input.bytes().len() == 0 {
-            process::exit(0);
+        let line = if use_readline {
+            readline_rep(&mut rl)
+        } else {
+            dumb_readline_rep()
+        };
+        match line {
+            Some(line) => {
+                if line.len() > 0 {
+                    rl.add_history_entry(&line);
+                    println!("{}", rep(line))
+                }
+            }
+            None => break
         }
-        input.pop(); // remove newline
-        println!("{}", rep(input));
+    }
+    rl.save_history(HISTORY_FILE).unwrap();
+}
+
+fn use_readline() -> bool {
+    env::var("READLINE").unwrap_or("true".to_string()) == "true"
+}
+
+fn readline_rep(rl: &mut Editor<()>) -> Option<String> {
+    let readline = rl.readline("user> ");
+    match readline {
+        Ok(line) => {
+            Some(line)
+        },
+        Err(ReadlineError::Interrupted) => {
+            None
+        },
+        Err(ReadlineError::Eof) => {
+            None
+        },
+        Err(err) => {
+            println!("Error: {:?}", err);
+            None
+        }
+    }
+}
+
+fn dumb_readline_rep() -> Option<String> {
+    print!("user> ");
+    stdout().flush().unwrap();
+    let mut line=String::new();
+    stdin().read_line(&mut line).unwrap();
+    if line.bytes().len() > 0 {
+        line.pop(); // remove newline
+        Some(line)
+    } else {
+        None
     }
 }
 
