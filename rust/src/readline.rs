fd34967e242e5fe49e04b8d3a519d7e0d550d267
Tim Morgan <tim@timmorgan.org>
2018-06-20 22:44:53 -0500

    Simplify readline module
    We don't need the dumb_readline code ever since switching to linefeed
    from readline. The actual readline library was inserting escape
    characters in the prompt that the mal tests did not recognize.

--- rust/src/readline.rs
+++ rust/src/readline.rs
@@ -1,10 +1,7 @@
-use std::io::{stdin, stdout, Write};
-use std::env;
 use linefeed::{DefaultTerminal, Interface, ReadResult};
 
 pub struct Readline {
     reader: Interface<DefaultTerminal>,
-    use_readline: bool,
 }
 
 const HISTORY_FILE: &str = ".mal-history";
@@ -16,16 +13,11 @@ impl Readline {
         reader.load_history(HISTORY_FILE).unwrap_or(());
         Readline {
             reader: reader,
-            use_readline: use_readline(),
         }
     }
 
     pub fn get(&mut self) -> Option<String> {
-        if self.use_readline {
-            real_readline(&mut self.reader)
-        } else {
-            dumb_readline()
-        }
+        readline(&mut self.reader)
     }
 
     pub fn save_history(&self) {
@@ -33,11 +25,7 @@ impl Readline {
     }
 }
 
-fn use_readline() -> bool {
-    env::var("READLINE").unwrap_or("true".to_string()) == "true"
-}
-
-fn real_readline(reader: &mut Interface<DefaultTerminal>) -> Option<String> {
+fn readline(reader: &mut Interface<DefaultTerminal>) -> Option<String> {
     match reader.read_line() {
         Ok(read_result) => match read_result {
             ReadResult::Input(line) => {
@@ -52,18 +40,5 @@ fn real_readline(reader: &mut Interface<DefaultTerminal>) -> Option<String> {
             println!("Error: {:?}", err);
             None
         }
-    }
-}
-
-fn dumb_readline() -> Option<String> {
-    print!("user> ");
-    stdout().flush().unwrap();
-    let mut line = String::new();
-    stdin().read_line(&mut line).unwrap();
-    if line.bytes().len() > 0 {
-        line.pop(); // remove newline
-        Some(line)
-    } else {
-        None
     }
 }
